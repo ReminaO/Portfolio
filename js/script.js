a959fe6110dc3db94237f81a98df32da6fcfc4d6
ReminaO <oremina@hotmail.fr>
2022-01-25 18:16:14 +0100

    Ajout du fichier CV et ajout du fichier carousel.js

--- js/script.js
+++ js/script.js
@@ -12,32 +12,3 @@ icone.addEventListener('click', function () {
     icone.classList.toggle('fa-times')//Affichage de l'iconer 'fermer'
 })
 
-//Mise en place du caroussel
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
